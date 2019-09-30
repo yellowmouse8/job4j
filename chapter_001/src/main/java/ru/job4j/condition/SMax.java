144e2714d3d6ac6d505c17d7947f67ad353f3dd3
Pavel <yellowmouse8@protonmail.com>
2019-09-30 15:49:30 +0300

    Выполненная задача SMax!

--- chapter_001/src/main/java/ru/job4j/condition/SMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/SMax.java
@@ -1,6 +1,6 @@
 package ru.job4j.condition;
 
-public class sQqMax {
+public class SMax {
     public static int max(int first, int second, int third, int forth) {
         int result = forth;
         if (first > second) {
@@ -8,13 +8,13 @@ public class sQqMax {
                 if (first > forth) {
                     result = first;
                 }
+            } else if(third>forth){
+                result=third;
             }
         } else if (second > third) {
             if (second > forth) {
                 result = second;
             }
-        } else if (third > forth) {
-            result = third;
         }
         return result;
     }
---
+++ chapter_001/src/test/java/ru/job4j/condition/SMaxTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SMaxTest {
+    @Test
+    public void maxall2() {
+        int expected = 20;
+        int result = SMax.max(20,16,15,2);
+        assertThat(result, is(expected));
+    }
+
+}
+
