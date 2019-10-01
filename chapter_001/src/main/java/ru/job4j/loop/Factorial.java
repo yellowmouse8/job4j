9a0f7cf4c5aac6e01f8dd20417405454ac1cb3ab
Pavel <yellowmouse8@protonmail.com>
2019-10-01 21:12:08 +0300

    Factor

--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -1,12 +1,11 @@
 package ru.job4j.loop;
 
 public class Factorial {
-    public static int calc(int n) {
+    public  int calc(int n) {
         int result = 0;
         int y = 1;
         for (int x = 1; x <= n; x++) {
             y = y * x;
-            System.out.println("y = " + y);
         }
         result = y;
         return result;
