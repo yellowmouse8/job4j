0a879176830b0839bf958e5aff6f7249dc72e936
Pavel <yellowmouse8@protonmail.com>
2019-10-04 19:03:29 +0300

    Тест Стероиды

---
+++ chapter_001/src/test/java/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.loop;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FitnessTest {
+    @Test
+    public void ivanBolshe() {
+        Fitness fit = new Fitness();
+        int expected = 0;
+        int month = fit.calc(110, 100);
+        assertThat(month, is(expected));
+    }
+    @Test
+    public void ivanMenshe() {
+        Fitness fit = new Fitness();
+        int expected = 2;
+        int month = fit.calc(60, 100);
+        assertThat(month, is(expected));
+    }
+    @Test
+    public void ivanMenshe2(){
+        Fitness fit= new Fitness();
+        int expected= 4;
+        int month = fit.calc(20, 100);
+        assertThat(month, is(expected));
+    }
+}
