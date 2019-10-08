cfad23fb243c3a50840d9f4bd4dd25bb6d6681bc
Pavel <yellowmouse8@protonmail.com>
2019-10-08 14:04:59 +0300

    Check prime Number!

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,16 +2,11 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public boolean check(int num) {
-        boolean prime = false;
-        int dividers = 0;
-        for (int nol = 1; nol <= num; nol++) {
+        boolean prime = true;
+        for (int nol = 2; nol <=Math.sqrt(num); nol++) {
             if (num % nol == 0) {
-                dividers++;
-            }
-            if (dividers>2){
-                prime=false;
-            }else {
-                prime=true;
+                prime =false;
+                break;
             }
         }
         return prime;
