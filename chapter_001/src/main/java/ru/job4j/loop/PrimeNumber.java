3a2a76a5aa42f8e89f953b85363680524b9f619a
Pavel <yellowmouse8@protonmail.com>
2019-10-08 15:59:10 +0300

    Prime Number

---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,22 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+
+    public static int isPrimeUp(int finish) {
+        int count = 0;
+        int dividers = 0;
+        boolean bee = true;
+        for (int x = 2; x <=finish; x++) {
+            bee=true;
+            for (int y = 2; y <=x; y++) {
+                if (x % y == 0) {
+                    bee = false;
+                    break;
+                }if (bee==true) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
