0a5cc22110a101800c04a1cb49079d10879fb732
Pavel <yellowmouse8@protonmail.com>
2019-10-09 20:17:57 +0300

    Счет кредита за год

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public  int year (int credit, int zarplata, double procent){
+        int year=0;
+        double procent1=procent/100;
+        double paymentYear=credit*procent1+credit;
+        while (zarplata<=paymentYear){
+            paymentYear=((paymentYear-zarplata)*procent1)+(paymentYear-zarplata);
+            year++;
+        }
+        return year;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/MortgageTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.loop;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class MortgageTest {
+    @Test
+    public void credit50(){
+        Mortgage mortgage= new Mortgage();
+        int year=mortgage.year(120,70,50);
+        assertThat(year, is(4));
+    }
+    @Test
+    public void credit25(){
+        Mortgage mortgage=new Mortgage();
+        int year=mortgage.year(170,90, 25);
+        assertThat(year,is(2));
+    }
+    @Test
+    public void amerikanskyCredit3n5(){
+        Mortgage mortgage=new Mortgage();
+        int year =mortgage.year(52300, 26053,3.7);
+        assertThat(year,is(2));
+    }
+}
