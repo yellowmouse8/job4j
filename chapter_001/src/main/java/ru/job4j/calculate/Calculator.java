2c8c80384e7af365a312950a26903a9161d91435
Pavel <yellowmouse8@protonmail.com>
2019-10-10 22:01:06 +0300

    Коммит JavaDoc

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculator.java
@@ -0,0 +1,65 @@
+/**
+ * Пакет для вычисления
+ */
+package ru.job4j.calculate;
+
+/**
+ * Class Класс для вычисления арифметических операций
+ * @author Pavel Shah
+ * @since 2.10
+ * @version 3.0
+ */
+public class Calculator {
+    /**
+     * Method add вычилсит upiter plus mars , выведет на консоль готовое выражение.
+     * @param upiter parametr1
+     * @param mars parametr2
+     */
+    public static void add(double upiter, double mars){
+        double result= upiter+mars;
+        System.out.println(upiter+"+"+mars+"="+result);
+    }
+
+    /**
+     * Method - div вычислит upiter divided mars, выведет на консоль готовое выражение.
+     * @param upiter parametr1
+     * @param mars parametr2
+     *
+     */
+    public static void div(double upiter, double mars){
+        double result= upiter/mars;
+        System.out.println(upiter+"/"+mars+"="+result);
+    }
+
+    /**
+     * Method - multiply вычислит upiter times mars, выведет на консоль готовое выражение.
+     * @param upiter parametr1
+     * @param mars parametr2
+     */
+    public static void multiply(double upiter, double mars){
+        double result=upiter*mars;
+        System.out.println(upiter+"*"+mars+"="+result);
+    }
+
+    /**
+     * Method subtrack вычислит upiter minus mars, выведет на консоль готовое выражение.
+     * @param upiter parametr1
+     * @param mars parametr2
+     */
+    public static void subtrack(double upiter, double mars){
+        double result=upiter-mars;
+        System.out.println(upiter+"-"+mars+"="+result);
+    }
+
+    /**
+     * Main, вывод строк со всеми готовыми арифметическими выражениями
+     * @param args - args.
+     */
+    public static void main(String[]args){
+        add(1,1);
+        add(2,2);
+        div(4,2);
+        multiply(2,1);
+        subtrack(10,5);
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/info.java
@@ -0,0 +1,20 @@
+/**
+ * Пакет для вывода строк
+ */
+package ru.job4j.calculate;
+
+/**
+ * Class - info для выполнения операции вывода строки на консоль.
+ * @author Pavel Shah
+ * @since 10.10
+ * @version 3.0
+ */
+public class info {
+    /**
+     * Main вывод на консоль строки содержащую только буквенные символы.
+     * @param args - args
+     */
+    public static void main(String[]args){
+        System.out.println(" Java передает всем 'привет' в мир людей");
+    }
+}
