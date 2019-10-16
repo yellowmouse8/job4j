703938a7bf54a7c8331f85d6813ce0dfa5055f51
Pavel <yellowmouse8@protonmail.com>
2019-10-16 19:08:40 +0300

    Коммит поиск эелементов в массиве.

---
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FindLoopTest {
+    @Test
+    public void whenArray5then0 (){
+        FindLoop find= new FindLoop();
+        int [] imput=new int[]{3,10,10,5};
+        int value=5;
+        int result=find.indexOf(imput,value);
+        int expected=3;
+        assertThat(result,is(expected));
+    }
+    @Test
+    public void whenArray12thenNull(){
+        FindLoop find=new FindLoop();
+        int[] imput=new int[]{1,3,6,10};
+        int value=5;
+        int result=find.indexOf(imput, value);
+        int expected = -1;
+        assertThat(result,is(expected));
+    }
+}
