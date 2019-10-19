8216aec6458b96da9d357c098ce5b283faf141e6
Pavel <yellowmouse8@protonmail.com>
2019-10-19 21:35:05 +0300

    Массив заполнен True or False

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = false;
-        for (int index = 0; index != pref.length; index++) {
+        for (int index = 0; index != word.length-pref.length; index++) {
             result=false;
             for (int index2 = 0; index2 != pref.length; index2++) {
                 if (pref[index2] == word[index]) {
--- chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -7,14 +7,14 @@ public class ArrayCharTest {
     @Test
     public void whenStartsWithPrefix(){
         char[] word= new char[]{'H','e','l','l','o'};
-        char [] pref=new char[]{'H','e'};
+        char [] pref=new char[]{'H','e','l'};
         boolean result=ArrayChar.startsWith(word,pref);
         assertThat(result,is(true));
     }
     @Test
     public void whenNotStatsWithPrefix(){
         char[] word=new char[]{'H', 'e', 'e', 'l', 'o'};
-        char [] pref=new char[]{'H','a','s'};
+        char [] pref=new char[]{'H','a'};
         boolean result=ArrayChar.startsWith(word,pref);
         assertThat(result,is(false));
     }
