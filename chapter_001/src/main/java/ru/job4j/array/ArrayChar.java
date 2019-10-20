5e82cdfc2972f8eddc0bdb8be1eebafbd0a8a87d
Pavel <yellowmouse8@protonmail.com>
2019-10-20 17:51:25 +0300

    Массив заполнен StartWith

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,13 +3,12 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = false;
-        for (int index = 0; index != word.length-pref.length; index++) {
-            result=false;
-            for (int index2 = 0; index2 != pref.length; index2++) {
-                if (pref[index2] == word[index]) {
-                    result = true;
-                    break;
-                }
+        for (int index = 0; index != word.length - pref.length; index++) {
+            result = true;
+            if (pref[index] != word[index]) {
+                result = false;
+                break;
+
             }
 
         }
