fd70475e374e0949145ed37ae61b2e2fba00b80f
Pavel <yellowmouse8@protonmail.com>
2019-10-22 17:28:41 +0300

    Сортировка массива перебором

--- chapter_001/src/main/java/ru/job4j/array/SortSelected.java
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -4,16 +4,17 @@ package ru.job4j.array;
 public class SortSelected {
 
     public static int[] sort(int[] data) {
-        for (int index = data.length - 1; index > 0; index--) {
-            for (int index2 = 0; index2 != index; index2++) {
-                if (data[index2] > data[index2 + 1]) {
-                    int tmp = data[index2];
-                    data[index2] = data[index2 + 1];
-                    data[index2 + 1] = tmp;
+        for (int index = 0; index < data.length; index++) {
+            int min = index;
+            for (int index2 = index + 1; index2 < data.length; index2++) {
+                if (data[index2] < data[min]) {
+                    min = index2;
                 }
             }
+            int tmp = data[min];
+            data[min] = data[index];
+            data[index] = tmp;
         }
-
         return data;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/sortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/sortTest.java
@@ -8,9 +8,9 @@ import static org.junit.Assert.assertThat;
 public class sortTest {
     @Test
     public void findMinAndMax() {
-        int[] index = new int[]{12, 124, 5, 55, 23, 46, 7, 2, 10,};
+        int[] index = new int[]{22, 34, 5, 55, 23, 46, 7, 2, 10,};
         int [] result = SortSelected.sort(index);
-        int [] expected = new int[]{2,5,7,10,12,23,46,55,124};
+        int [] expected = new int[]{2,5,7,10,22,23,34,46,55};
         assertThat(result, is(expected));
     }
 }
