485ced8f8d002278016dd7fc26cdf0063e88336a
Pavel <yellowmouse8@protonmail.com>
2019-10-22 20:55:06 +0300

    Двумерная матрица Таблица умножения.

---
+++ chapter_001/src/main/java/ru/job4j/array/Mattrix.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class Mattrix {
+    public int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for (int index = 0; index < size; index++) {
+            for (int index2 = 0; index2 < size; index2++) {
+                table[index][index2] = (index + 1) * (index2 + 1);
+            }
+
+        }
+        return table;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/MatrixTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MatrixTest {
+    @Test
+    public void whenTableIs9(){
+        Mattrix mattrix=new Mattrix();
+        int [][] table = mattrix.multiple(3);
+        int [][] expected = new int[][] {
+                {1,2,3},
+                {2,4,6},
+                {3,6,9}
+        };
+        assertThat(table, is(expected));
+    }
+}
