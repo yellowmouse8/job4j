b74fef4d1cc86f3df1ef63f0abd3dea3dc692cee
Pavel <yellowmouse8@protonmail.com>
2019-11-07 21:29:31 +0300

    Демонстрация полей

--- chapter_001/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_001/src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,26 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String  name;
+
+    public void show(){
+        System.out.println(" Here's " + this.name + " , and he is eating  "+ this.food);
+    }
+    public void eat (String meat){
+        this.food=meat;
+    }
+    public void giveNick (String nick){
+        this.name=nick;
+    }
+
     public static void main (String[]args){
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        Cat cat = new Cat();
+        cat.giveNick(" Gav ");
+        cat.eat(" Hamon ");
+        cat.show();
+        cat.giveNick(" Black ");
+        cat.eat(" Cheese ");
+        cat.show();
     }
 }
