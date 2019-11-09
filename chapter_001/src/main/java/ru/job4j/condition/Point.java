7842f7f0ef8ec1d537a87eec0ffd36ee6dbcc858
Pavel <yellowmouse8@protonmail.com>
2019-11-09 20:33:09 +0300

    Рефакторинг класса Point

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,12 +1,22 @@
 package ru.job4j.condition;
 
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return Math.sqrt(Math.pow((this.x - that.x), 2) + Math.pow((this.y - that.y), 2));
     }
 
     public static void main(String[] args) {
-        double result = distance(1, 2, 6, 6);
-        System.out.println(" Результат точек в системе координат (6-1) и (6-2) это " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(" Результат точек в системе координат (6-1) и (6-2) это " + dist);
     }
 }
