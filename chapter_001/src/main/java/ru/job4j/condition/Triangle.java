e458d05468958f3dc2b225e6ec1accb53fb0124c
Pavel <yellowmouse8@protonmail.com>
2019-11-09 23:54:08 +0300

    Треугольник 4

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -1,10 +1,39 @@
 package ru.job4j.condition;
 
 public class Triangle {
-    public static boolean exist(double ab, double ac, double dc) {
-        if (ab + ac > dc && ac + dc > ab && ab + ab + dc > ac) {
-            return true;
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point a, Point b, Point c) {
+        this.first = a;
+        this.second = b;
+        this.third = c;
+    }
+
+    public double period(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public double area() {
+        double rsl = -1;
+        double a = first.distance(second);
+        double b = first.distance(third);
+        double c = second.distance(third);
+        double p = period(a, b, c);
+
+        if (exist(a, b, c)) {
+
+            rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         }
-        return false;
+        return rsl;
+    }
+
+    public boolean exist(double a, double b, double c) {
+        if (a != 0 && b != 0 && c != 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -9,15 +9,36 @@ import static org.junit.Assert.assertThat;
 
 @Ignore
 public class TriangleTest {
-    @Test
-    public void kogdaSushestvuet() {
-        boolean result = Triangle.exist(3 + 3, 3, 8);
-        Assert.assertThat(result, is(true));
+        @Test
+        public void checkS () {
+            Point a = new Point(2, 7);
+            Point b = new Point(4, 6);
+            Point c = new Point(2, 6);
+            Triangle triangle = new Triangle(a, b, c);
+            double result = triangle.area();
+            double expected = 1;
+            assertThat(result, is(expected));
+
+        }
+        @Test
+    public void checkTrue(){
+            Point a = new Point(1, 3);
+            Point b = new Point(2, 4);
+            Point c = new Point(1, 2);
+            Triangle triangle = new Triangle(a,b,c);
+            boolean result = triangle.exist( 1,  2,3);
+            boolean expected = true;
+            assertThat( result, is(expected));
+        }
+        @Test
+    public void checkPerimetr (){
+            Point a = new Point(1, 3);
+            Point b = new Point(2, 4);
+            Point c = new Point(1, 2);
+            Triangle triangle = new Triangle(a,b,c);
+            double result = triangle.period(1,2,3);
+            double expected = 3;
+            assertThat(result , is(expected));
+        }
     }
 
-    @Test
-    public void kogdanesushestvuet() {
-        boolean result = Triangle.exist(0.0, 0.0, 0.0);
-        Assert.assertThat(result, is(false));
-    }
-}
