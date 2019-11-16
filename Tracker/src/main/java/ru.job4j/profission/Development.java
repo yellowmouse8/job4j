9526b4f2c77c6efcbac7283223a8f6564ea9c53d
Pavel <yellowmouse8@protonmail.com>
2019-11-16 21:28:13 +0300

    Иерархия профессий

--- Tracker/src/main/java/ru.job4j/profission/Development.java
+++ Tracker/src/main/java/ru.job4j/profission/Development.java
@@ -1,10 +1,13 @@
 package ru.job4j.profission;
 
 public class Development extends Programmer {
-    public String development;
+    private String development;
 
     public Development(String name, String surname, String education, double birthday, String development) {
         super(name, surname, education, birthday);
+    }
+
+    public void setDevelopment(String development) {
         this.development = development;
     }
 
