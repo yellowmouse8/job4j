e33a98ef2a2c252e3df0261b1e66c66e18fab5bc
Pavel <yellowmouse8@protonmail.com>
2019-12-08 01:32:34 +0300

    SingleTon Test

--- Tracker/src/test/java/ru/job4j/singleton/SingleTonTest.java
+++ Tracker/src/test/java/ru/job4j/singleton/SingleTonTest.java
@@ -7,6 +7,7 @@ import org.junit.After;
 import ru.job4j.poly.TrackerEagerLoad;
 import ru.job4j.poly.TrackerInner;
 import ru.job4j.poly.TrackerSingleTon;
+import ru.job4j.poly.TrackerSingleTonLazy;
 import ru.job4j.start.Tracker;
 
 import java.io.ByteArrayOutputStream;
@@ -14,13 +15,35 @@ import java.io.PrintStream;
 
 
 public class SingleTonTest {
+    private static TrackerSingleTon trackerSingleTon;
+    private  static TrackerSingleTonLazy trackerSingleTonLazy;
+    private static TrackerEagerLoad trackerEagerLoad;
+    private static TrackerInner trackerInner;
+
 
     @Test
-    public void whenOneSingleTon() {
-
+    public void whenEnumSingleTon() {
         TrackerSingleTon trackerSingleTon = TrackerSingleTon.ADD;
+        TrackerSingleTon trackerSingleTon1 = TrackerSingleTon.ADD;
+        assertThat(trackerSingleTon == trackerSingleTon1, is(true));
+    }
+    @Test
+    public void whenStaticLazySingle (){
+        TrackerSingleTonLazy trackerSingleTonLazy = TrackerSingleTonLazy.getLazy();
+        TrackerSingleTonLazy trackerSingleTonLazy1 = TrackerSingleTonLazy.getLazy();
+        assertThat(trackerSingleTonLazy == trackerSingleTonLazy1, is(true));
+    }
+    @Test
+    public void whenStaticFinalEager (){
         TrackerEagerLoad trackerEagerLoad = TrackerEagerLoad.getEagerLoad();
-        assertThat(trackerSingleTon.findAll(), is(trackerEagerLoad.findAll()));
+        TrackerEagerLoad trackerEagerLoad1 = TrackerEagerLoad.getEagerLoad();
 
+        assertThat(trackerEagerLoad == trackerEagerLoad1, is(true));
+    }
+    @Test
+    public void whenPrivateStaticFinalLazySingle(){
+        TrackerInner trackerInner = TrackerInner.getTrackerInner();
+        TrackerInner trackerInner1 = TrackerInner.getTrackerInner();
+        assertThat(trackerInner == trackerInner1, is(true));
     }
 }
