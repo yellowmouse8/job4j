6ed2d6188a4e9a7b2d237bbe01955b99dfa4f25e
Pavel <yellowmouse8@protonmail.com>
2019-12-16 22:59:43 +0300

    Телефонный справочник ArrayLst

---
+++ Tracker/src/test/java/ru/job4j/collection/PhoneDictionaryTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+import org.junit.Test;
+import java.util.ArrayList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void findByName (){
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Pavel", "Shah", "+9943434", "Moscow"));
+        ArrayList<Person> persons = phones.find("Moscow");
+        assertThat(persons.get(0).getName(), is("Pavel"));
+    }
+}
