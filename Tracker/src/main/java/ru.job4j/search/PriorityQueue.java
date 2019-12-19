3a3d8c49c09591e7cb923f503869d880e539f588
Pavel <yellowmouse8@protonmail.com>
2019-12-19 18:41:51 +0300

    Приоритет ArrayList

--- Tracker/src/main/java/ru.job4j/search/PriorityQueue.java
+++ Tracker/src/main/java/ru.job4j/search/PriorityQueue.java
@@ -1,25 +1,33 @@
 package ru.job4j.search;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class PriorityQueue {
-    private LinkedList<Task>  tasks = new LinkedList<>();
+    private LinkedList<Task> tasks = new LinkedList<>();
 
     public void put(Task task) {
-        if (tasks.size() == 0) {
-            tasks.add(task);
-        } else {
-            for (Task element : tasks) {
-                if (task.getPriority() < tasks.element().getPriority()) {
-                    tasks.add(0, task);
+        if (tasks.size() > 0) {
+            for (int index = 0; index != tasks.size(); index++) {
+                if (task.getPriority() < tasks.get(index).getPriority()) {
+                    tasks.add(index, task);
+                    break;
+                } else if (task.getPriority() == tasks.get(index).getPriority()) {
+                    tasks.add(index + 1, task);
+                    break;
+                }
+                if (index == tasks.size() - 1) {
+                    tasks.add(task);
                     break;
                 }
             }
+        } else {
+            tasks.add(task);
         }
     }
 
     public Task take() {
-        return tasks.remove(0);
+        return tasks.poll();
     }
 }
 
--- Tracker/src/test/java/ru/job4j/search/PriorityQueueTEst.java
+++ Tracker/src/test/java/ru/job4j/search/PriorityQueueTEst.java
@@ -1,26 +1,29 @@
 package ru.job4j.search;
+
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class PriorityQueueTEst {
-@Test
-    public void whenHigherPriority (){
-    PriorityQueue queue = new PriorityQueue();
-    queue.put(new Task("low", 3));
-    queue.put(new Task("urgent", 1));
-    queue.put(new Task("middle", 4));
-    Task result = queue.take();
-    assertThat(result.getDesc(), is("urgent"));
-}
-@Test
-    public void whenSamePriority (){
-    PriorityQueue queue = new PriorityQueue();
-    queue.put(new Task("finish", 7));
-    queue.put(new Task("start", 6));
-    queue.put(new Task("middle", 3));
-    Task result = queue.take();
-    assertThat(result.getPriority(), is(3));
-}
+    @Test
+    public void whenHigherPriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("low", 3));
+        queue.put(new Task("urgent", 1));
+        queue.put(new Task("middle", 4));
+        Task result = queue.take();
+        assertThat(result.getDesc(), is("urgent"));
+    }
+
+    @Test
+    public void whenSamePriority() {
+        PriorityQueue queue = new PriorityQueue();
+        queue.put(new Task("finish", 7));
+        queue.put(new Task("start", 6));
+        queue.put(new Task("middle", 3));
+        Task result = queue.take();
+        assertThat(result.getPriority(), is(3));
+    }
 }
