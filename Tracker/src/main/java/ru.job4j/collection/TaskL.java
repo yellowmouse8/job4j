3246189d6d8f26c885474fc3435c704f659e58ec
Pavel <yellowmouse8@protonmail.com>
2019-12-21 23:51:39 +0300

    HashSet table

--- Tracker/src/main/java/ru.job4j/collection/TaskL.java
+++ Tracker/src/main/java/ru.job4j/collection/TaskL.java
@@ -1,5 +1,7 @@
 package ru.job4j.collection;
 
+import java.util.Objects;
+
 public class TaskL {
     private String number;
     private String description;
@@ -8,6 +10,29 @@ public class TaskL {
         this.number = number;
         this.description = description;
     }
+
+    @Override
+    public String toString() {
+        return "TaskL{" +
+                "number='" + number + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TaskL taskL = (TaskL) o;
+        return Objects.equals(number, taskL.number) &&
+                Objects.equals(description, taskL.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number, description);
+    }
+
     public String getNumber(){
         return number;
     }
--- Tracker/src/main/java/ru.job4j/collection/TaskOrder.java
+++ Tracker/src/main/java/ru.job4j/collection/TaskOrder.java
@@ -4,27 +4,30 @@ import ru.job4j.search.Task;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 public class TaskOrder {
 
 
-        public HashSet<TaskL> find (HashSet<TaskL> taskLS) {
-            HashSet<TaskL> result = new HashSet<>();
-            for (TaskL value : taskLS) {
-                result.add(value);
-
-                return result;
+        public HashSet<TaskL> find (List<TaskL> taskLS) {
+            HashSet<TaskL> taskLHashSet = new HashSet<>();
+            if (taskLS != null) {
+                for (TaskL value : taskLS) {
+                    taskLHashSet.add(value);
+                }
             }
+                return taskLHashSet;
+
         }
 
     public static void main (String[]args){
         TaskOrder order = new TaskOrder();
-        HashSet <TaskL> oi = new HashSet<>();
+        List <TaskL> oi = new ArrayList<>();
         oi.add(new TaskL("12", " Bi"));
         oi.add(new TaskL("2", " UF"));
         oi.add(new TaskL("2", " UF"));
         oi.add(new TaskL("2", " UF"));
         order.find(oi);
-        System.out.println(oi);
+        System.out.println(order.find(oi));
     }
 }
