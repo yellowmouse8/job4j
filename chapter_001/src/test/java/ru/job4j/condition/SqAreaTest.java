70c648c214324f55903abed7306956e88f8313ba
Pavel <yellowmouse8@protonmail.com>
2020-01-05 17:34:20 +0300

    Correct files

--- chapter_001/src/test/java/ru/job4j/condition/SqAreaTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class SqAreaTest {
     @Test
