4aee19c6f349144900fd39dfd3ab7e90d8a3de75
Pavel <yellowmouse8@protonmail.com>
2020-01-05 17:50:22 +0300

    Correct files

--- chapter_001/src/test/java/ru/job4j/condition/sqAreaTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/sqAreaTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
-public class SqAreaTest {
+public class sqAreaTest {
     @Test
     public void when4k1s1() {
         int k = 1;
